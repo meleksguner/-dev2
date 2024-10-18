f52a3d02dab23f9c9eaff3a740d3a0336b7afcdd
meleksguner <msenaguner34@gmail.com>
2024-10-19 01:05:18 +0300

    Add files via upload

---
+++ if else admin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Kullanıcı adınızı girin:");
+        string kullaniciadi = Console.ReadLine();
+
+        Console.WriteLine("Şifrenizi girin:");
+        string sifre = Console.ReadLine();
+
+        if (kullaniciadi == "admin" && sifre == "admin_32453")
+            Console.WriteLine("Giriş Başarılı!");
+
+        else
+            Console.WriteLine("Giriş Başarısız!");
+        
+        Console.ReadKey();
+    }
+}
