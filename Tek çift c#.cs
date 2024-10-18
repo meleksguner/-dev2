820675c474aecc26f7289b881b8b10c3ca1c1b9e
meleksguner <msenaguner34@gmail.com>
2024-10-19 00:10:43 +0300

    Add files via upload

---
+++ Tek çift c#.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Bir sayı giriniz: ");
+        int a = int.Parse(Console.ReadLine());
+
+        if (a % 2 == 0)
+            Console.WriteLine("Sayı çift sayıdır.");
+
+        else
+            Console.WriteLine("Sayı tek sayıdır. ");
+
+        Console.ReadKey();
+    }
+  
+}
+
